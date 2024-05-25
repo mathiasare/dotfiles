f6b9cdaa67e884c3f022f1ca338e712b7cfe1c81
mathiasare <55914048+mathiasare@users.noreply.github.com>
2024-05-25 02:02:21 +0200

    latest

---
+++ .local/bin/run_zi.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+selected=$(zoxide query -i)
+
+if [[ -z $selected ]]; then
+    echo "selected not found"
+    exit 0
+fi
+
+selected_name=$(basename "$selected" | tr . _)
+tmux_running=$(pgrep tmux)
+
+if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
+    tmux new-session -s $selected_name -c $selected
+    exit 0
+fi
+
+if ! tmux has-session -t=$selected_name 2> /dev/null; then
+    tmux new-session -ds $selected_name -c $selected
+fi
+
+tmux switch-client -t $selected_name
